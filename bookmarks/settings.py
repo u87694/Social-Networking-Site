422d63c4d5dcce8026b2e494558bcf201103fac7
priyanshu-pj <homepc20140422@outlook.com>
2022-12-05 14:22:16 +0530

    create Profile for social authentication

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -151,3 +151,18 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = "396990931495-rv5lecvcaqqgmma2brk6870tliregbbl.apps.googleusercontent.com"
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "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"
+
+
+# Social Auth Pipeline
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
