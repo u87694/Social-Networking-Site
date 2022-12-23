80d794ebf28fecc0824f3f4094e61455adb80bdb
priyanshu-pj <homepc20140422@outlook.com>
2022-12-23 12:39:56 +0530

    Images list view functional

--- images/views.py
+++ images/views.py
@@ -1,4 +1,5 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_POST
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
@@ -47,3 +48,32 @@ def image_like(request):
             pass
         
     return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # render the first page if page is not an integer
+        images = paginator.page(1)
+    except EmptyPage:
+        # if requested page does not exists
+        if images_only:
+            # if AJAX is requests, return resposne to the HTML
+            return HttpResponse('')
+            # if page number is requested return last page
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request, 'images/image/list_images.html', {
+            'section': 'images', 'images': images
+        })
+
+    return render(request, 'images/image/list.html', {
+        'section': 'images', 'images': images
+    })
