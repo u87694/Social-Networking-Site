58a09332a5d2bcb4750c738a6e330b9dd45ccc41
u87694 <98472205+u87694@users.noreply.github.com>
2023-12-28 16:14:26 +0530

    fixes for production

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -14,8 +14,8 @@ SECRET_KEY = 'django-insecure-jn5t@vlxsaoau99wfn_s3%k5&56z*321bq6h+8bsl=-5by@hlk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1"]
-
+# ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['social-networking-site-wj6u.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
